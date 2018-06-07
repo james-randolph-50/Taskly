eec94248507e56ac0569e8ecda50841b8bc14d86
James <james.randolph2016@gmail.com>
2018-06-07 19:05:37 -0400

    added http requests to lists controller:

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def list_params
+    end
+
+    def find_list
+    end
+    
 end
