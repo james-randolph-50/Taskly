3119a18f8f86fd09bb70b243abc160cdf7b3e600
James <james.randolph2016@gmail.com>
2018-06-08 15:23:27 -0400

    set controller actions in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def set_user
+    end
+
+    def user_params
+    end
 end
