083667a9d8bd074f74ae58ec3ec530de1c730ddf
James <james.randolph2016@gmail.com>
2018-06-08 15:30:37 -0400

    create function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,17 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+        respond_to do |format|
+            if @user.save
+                session[:user_id] = @user.id
+                format.html { redirecto_to User_path(@user), notice: "Welcome to Taskly" }
+            else
+                format.html { render :new }
+            end
+        end
     end
-    
+
 
     def edit
     end
