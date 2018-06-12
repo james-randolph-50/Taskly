e873ef1c11d5f6b0cfa46ce8a0edba4c3fb40cfd
James <james.randolph2016@gmail.com>
2018-06-12 18:52:04 -0400

    post screenshare; need to redo project on Ubuntu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :lists
+  resources :users
+  resources :tasks
   #devise_for :users
   get 'welcome/home'
+  get 'signin', to: 'sessions#create', as: 'signin'
+  get 'signup', to: 'users#create', as: 'signup'
 
   authenticated :user do
     root "lists#index", as: "authenticated_root"
