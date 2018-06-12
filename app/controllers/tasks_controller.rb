b50bb653da646d93feb6fa059997dca7df880556
James <james.randolph2016@gmail.com>
2018-06-12 18:10:12 -0400

    create and update methods for Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,22 @@
 class TasksController < ApplicationController
 
     def create
+        t = Task.new(task_params)
+        t.save
+        redirect_to list_path(t.list)
     end
 
     def update
-    end
+        t = Task.find(params[:id])
+        if t.user == current_user
+          t.update(completion_status: 'complete', completion_date: DateTime.now)
+          t.list.update(last_completed: DateTime.now)
+        end
+        redirect_to :back
+      end
 
     def last_completed
+        @task = Task.last_completed
     end
     
 end
